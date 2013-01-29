6a252caa7f7085dbecbe810db689a7c45de87a39
syokenz <syokenz@gmail.com>
2013-01-29 23:02:11 +0900

    sample bukken for mongodb

--- app.js
+++ app.js
@@ -1,18 +1,107 @@
 var express = require('express')
   , routes = require('./routes');
 
+var mongo_server = 'mongo-server'
+var oracle_server = 'oracle-server'
+
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('hello world express!');
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
+
+app.get('/', function(req, res){
+  res.render('index', { title: 'Express' })
+});
+
+
+app.get('/mongo/bukken', function(req, res){
+  var mongo = require('mongodb'),
+    Server = mongo.Server,
+    Db = mongo.Db;
+  
+  var server = new Server(mongo_server, 27017, {});
+  var db = new Db('lw', server, {safe:true});
+  var user_id   = req.param('user_id');
+  var bukken_id = req.param('bukken_id');
+
+  var q = {};
+  q['bukken_id'] = parseInt(bukken_id);
+
+  db.open(function(err, db) {
+    db.collection('bukken', function(err, collection) {
+      collection.findOne(q, function(err, doc) {
+        //debug
+        for (var key in q) {
+          console.log('q = '+ key + ':'+q[key]);
+        }  
+        res.render('bukken', { user_id:   user_id,
+                               bukken_id: bukken_id,
+                               doc: doc })
+        db.close();
+      });
+    });
+  });
+});
+
+app.get('/mongo/api', function(req, res){
+  var mongo = require('mongodb'),
+    Server = mongo.Server,
+    Db = mongo.Db;
+  
+  var server = new Server(mongo_server, 27017, {});
+  var db = new Db('lw', server, {safe:true});
+  var user_id = req.param('user_id');
+  var api_id  = req.param('api_id');
+
+  var q = {};
+  q['user_id'] = parseInt(user_id);
+
+  db.open(function(err, db) {
+    db.collection('users', function(err, collection) {
+      collection.find(q).toArray(function(err, docs) {
+        //debug
+        for (var key in q) {
+          console.log('q = '+ key + ':'+q[key]);
+        }  
+        console.log('docs = ' + docs)
+        for (var doc in docs) {
+          console.log(doc)
+        }  
+
+        res.send(docs);
+        db.close();
+      });
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('/mongo', function(req, res){
   var mongo = require('mongodb'),
     Server = mongo.Server,
     Db = mongo.Db;
   
-  var server = new Server('localhost', 27017, {});
+  var server = new Server(mongo_server, 27017, {});
   var db = new Db('test', server, {safe:true});
   
   db.open(function(err, db) {
@@ -31,7 +120,7 @@ app.get('/mongo', function(req, res){
 app.get('/oracle', function(req, res){
   var oracle = require('db-oracle');
   new oracle.Database({
-      hostname: 'localhost',
+      hostname: oracle_server,
       user: 'test',
       password: 'test',
       database: 'XE'
@@ -51,4 +140,4 @@ app.get('/oracle', function(req, res){
   });
 });
 
-app.listen(3000);
+app.listen(8080);
